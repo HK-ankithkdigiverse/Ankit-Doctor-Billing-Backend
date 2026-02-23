69a100083e36c59e4947cff735688c9ff650f45f
HK-ankithkdigiverse <ankithkdigiverse@gmail.com>
2026-02-23 15:34:00 +0530

    ..

--- Medicine_Billing_App_backend/src/database/connection/index.ts
+++ Medicine_Billing_App_backend/src/database/connection/index.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
-import express from 'express'
 import { ensureCategoryCollectionIndexes } from '../models/category';
-const dbUrl: any = process.env.DB_URL;
-const mongooseConnection = express()
+const dbUrl = process.env.DB_URL as string;
+
 mongoose.set('strictQuery', false)
-mongoose.connect(
-    dbUrl
-).then(async () => {
-    console.log('Database successfully connected');
-    await ensureCategoryCollectionIndexes();
-}).catch(err => console.log(err));
+
+const mongooseConnection = mongoose
+    .connect(dbUrl)
+    .then(async () => {
+        console.log('Database successfully connected');
+        await ensureCategoryCollectionIndexes();
+    })
+    .catch((err) => console.log(err));
 
 export { mongooseConnection }
